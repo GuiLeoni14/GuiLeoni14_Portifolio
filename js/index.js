8ed3a7abbb09a6ea816e6187afbcc462ec338518
GuiLeoni14 <guileone167@gmail.com>
2021-10-18 14:23:27 -0200

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// const count = document.getElementById('number');
+// function callback (){
+//     cont_number = parseInt(5 + (Math.random()*1));
+//     count.textContent = Number(count.textContent) + cont_number;
+//     if (clearInterval(Number(count.textContent) >= 65)){
+//         clearInterval(intervalo);
+//     }
+// }
+// var intervalo = setInterval(callback, 300);
